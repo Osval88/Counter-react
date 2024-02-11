3aef43419a5fb1d79f3572728fbcb23c992efb2e
Osvaldo S. Lell <osvaldo_lell@yahoo.com>
2024-02-11 18:02:29 -0300

    Creamos un contador hook

---
+++ src/HooksApp.jsx
@@ -0,0 +1,12 @@
+import { ContadorComponent } from "./components/ContadorComponent"
+
+export const HooksApp = () => {
+    return (
+         <>
+           <h1>Contador</h1>
+           <hr />
+           <ContadorComponent></ContadorComponent>
+         </>
+    )
+}
+ 
